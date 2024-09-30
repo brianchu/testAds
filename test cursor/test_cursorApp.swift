da24d635d15baaf0e523db08937d0f7d754b74b2
Brian <sychu@yahoo.com>
2024-09-29 20:09:11 -0700

    Initial Commit

---
+++ test cursor/test_cursorApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_cursorApp.swift
+//  test cursor
+//
+//  Created by brian on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct test_cursorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ test cursorTests/test_cursorTests.swift
@@ -0,0 +1,17 @@
+//
+//  test_cursorTests.swift
+//  test cursorTests
+//
+//  Created by brian on 9/29/24.
+//
+
+import Testing
+@testable import test_cursor
+
+struct test_cursorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
